1272ef364bd51339afed805a5c6e12752a46fc41
Sajid Chowdhury <sajidahmed@iut-dhaka.edu>
2023-11-25 09:43:17 +0600

    logo changed

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,7 +13,7 @@ var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('pages/index', { title: 'HAE YEON KANG' });
+    res.render('pages/index', { title: 'Sajid Chowdhury' });
 });
 
 /* about me */
